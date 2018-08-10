91ffffc9a05f408487d558f4866f983b9062bab6
aytsang14@gmail.com <aytsang14@gmail.com>
2018-08-09 18:15:20 -0700

    Database created, Tables created with values. Displays items in stock from products table in bamazon database. Prompts user to ask which product they would like to buy and how much. Current writing code to check if products table has items in stock and if it does, subtract from table

---
+++ bamazonCustomer.js
@@ -0,0 +1,125 @@
+var mysql 	 = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  
+  // Your port; if not 3306
+  port: 3306,
+  
+  // Your username
+  user: "root",
+  
+  // Your password
+  password: "password",
+  database: "bamazon"
+});
+ 
+connection.connect();
+	// ids, names, and prices of products for sale.
+
+// this runs the first query of prompting the user with all the avaialabel products
+// After the query is run, we run teh slectPrompt function
+connection.query('SELECT item_id, product_name, department_name, price stock_quantity '+
+	'FROM products', function(err,res) {
+		if(err) throw err;
+		console.log('----------- Current Items -----------')
+		console.log(res);
+		selectPrompt();
+});
+
+// Once the customer has placed the order, your application 
+// should check if your store has enough of the product to meet the customer's request.
+
+// The selectPrompt function prompts the user to
+// enter an item id and the nhow many units of product they want
+function selectPrompt() {
+	inquirer.prompt([
+		{
+			type: 'input',
+			message: 'Enter the Item ID of the product you would like to buy',
+			name: 'idProduct'
+		},
+		{
+			type: 'input',
+			message: 'How many units of the product would you like to buy?',
+			name: 'purchaseAmount'
+		}
+		]).then(function(inquirerResponse) {
+			var id_product = inquirerResponse.idProduct;
+			var purchase_amount = inquirerResponse.purchaseAmount;
+			checkStock(id_product, purchase_amount);
+			
+		})
+} 
+
+
+// the checkStock function check if your store 
+// has enough of the product to meet the customer's request.
+function checkStock(productId, purchaseAmount) {
+	connection.query('SELECT stock_quantity, product_name FROM products WHERE item_id = '
+	 + productId
+	 ,function(err, res) {
+
+	 	if(err) throw err;
+
+	 	var stockQuantity = res[0].stock_quantity;
+	 	var productName   = res[0].product_name;
+
+	 	if(productId == '') {
+	 		
+	 		console.log('This is not a valid product Id');
+	 	
+	 	} else if(productId != '') {
+
+	 		if(stockQuantity < purchaseAmount) {
+
+	 			console.log('You requested '+purchaseAmount+' amount of '+productName+'.');
+	 			console.log('Unfortunately we only posses ' + stockQuantity
+	 				+ ' amount of '+productName+'.')
+
+	 		} else if (stockQuantity = 0) {
+
+	 			console.log('We do not have anymore of that product. Sorry!' + 
+	 				' Check with us soon!');
+
+	 		} else if (stockQuantity > purchaseAmount) {
+
+	 			console.log(purchaseAmount);
+
+	 		}
+
+	 	} else {
+	 		
+	 		console.log('something went wrong');
+
+	 	}
+	 	connection.end();
+	 })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
