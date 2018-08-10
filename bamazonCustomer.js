f76c7f5f455b6bff4c14a225508a907613661abc
aytsang14@gmail.com <aytsang14@gmail.com>
2018-08-10 11:33:34 -0700

    refined some functions into 1. Now updates the product_sales column as well.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -53,81 +53,85 @@ function selectPrompt() {
 // the checkStock function check if your store 
 // has enough of the product to meet the customer's request.
 function checkStock(productId, purchaseAmount) {
-
-	connection.query('SELECT stock_quantity, product_name, price FROM products WHERE item_id = '
+	connection.query('SELECT stock_quantity, product_name, price, product_sales FROM products WHERE item_id = '
 	 + productId
 	 ,function(err, res) {
 
 	 	if(err) throw err;
-
 	 	var stockQuantity = res[0].stock_quantity;
 	 	var productName   = res[0].product_name;
 	 	var price 		  = res[0].price;
 
-	 	if(productId == '') {
-	 		
+	 	if(productId == '') {	 		
 	 		console.log('This is not a valid product Id');
-	 	
 	 	} else if(productId != '') {
 
 	 		if(stockQuantity < purchaseAmount) {
-
 	 			console.log('You requested '+purchaseAmount+' amount of '+productName+'.');
 	 			console.log('Unfortunately we only posses ' + stockQuantity
 	 				+ ' amount of '+productName+'.')
 
 	 		} else if (stockQuantity == 0) {
-
 	 			console.log('We do not have anymore of that product. Sorry!' + 
 	 				' Check with us soon!');
 
 	 		} else if (stockQuantity >= purchaseAmount) {
+	 			var newQuantity 	= stockQuantity - purchaseAmount;
+	 			var total 			= purchaseAmount * price;
+	 			var productSet 		= 'product_sales = product_sales + ' + total;
+	 			var where			= 'item_id = ' + productId;
+	 			var stockSet	    = 'stock_quantity = ' + newQuantity;
 
-	 			var newQuantity = stockQuantity - purchaseAmount;
-	 			var total 		= purchaseAmount * price;
-
-	 			console.log('================');
-	 			updateQuantity(productId, newQuantity);
-	 			console.log('================');
+	 			updateProductSales(stockSet, where, false);
+	 			updateProductSales(productSet, where, true);
 	 			console.log('Thank you for your purchase!');
 	 			console.log('Your total will be $' + total);
-
 	 		} 
-	 		
 	 	} 
 	 	else {
-	 		
 	 		console.log('something went wrong');
 
 	 	}
 	 })
 }
 
-// this function takes in 2 arguements.
-// the first argument gets the specific item id in order to 
-// target the correct row.
-// the second argument takes in the price to update.
-function updateQuantity(rowId, newQuantity) {
-	
-	console.log('================');
-	connection.query('UPDATE products SET stock_quantity = '
-		+newQuantity+' WHERE item_id ='+rowId
+
+// this function takes in 3 arguements.
+// the first argument is used to specify the SET query
+// the second argument is used to specify the WHERE query
+// the 3rd argument used to see if we should still be connected after 
+// calling this function.
+function updateProductSales(setVariable, whereVariable, connect) {
+	console.log('Line 125 is happening');
+	connection.query('UPDATE products SET ' + setVariable + ' WHERE ' + whereVariable
 		,function(err,res) {
 			if(err) throw err;
-			connection.end();
-		})
-
+			if(connect)connection.end();
+		}
+	)
 }
 
 
 
 
 
+// this function takes in 2 arguements.
+// the first argument gets the specific item id in order to 
+// target the correct row.
+// the second argument takes in the price to update.
+// function updateQuantity(rowId, newQuantity) {
+// 	console.log('================');
+// 	connection.query('UPDATE products SET stock_quantity = '
+// 		+newQuantity+' WHERE item_id ='+rowId
+// 		,function(err,res) {
+// 			if(err) throw err;
+// 			connection.end();
+// 		}
+// 	)
+// }
 
-
-
-
-
+// UPDATE products SET stock_quantity = '+newQuantity+' WHERE item_id ='+rowId';
+// UPDATE products SET product_sales = product_sales + 5  WHERE item_id = 1010;
 
 
 
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,6 +1,13 @@
 var mysql 	 = require('mysql');
 var inquirer = require('inquirer');
 
+var viewInventoryQuery 	  = 'SELECT item_id, product_name, department_name, price, stock_quantity '+
+	'FROM products';
+var viewInventoryMsg 	  = '----------- Current Items -----------';
+var viewLowInventoryQuery = 'SELECT * FROM products WHERE stock_quantity <= 200';
+var viewLowInventoryMsg   = '----------- Current Items -----------';
+
+
 var connection = mysql.createConnection({
   host: "localhost",
   
@@ -23,51 +30,51 @@ inquirer.prompt([
 		'Add to Inventory','Add New Product'],
 		name: 'menu'
 	}
-	]).then(function(inquireResponse) {
-		console.log(inquireResponse.menu);
-		runOption(inquireResponse.menu);
+]).then(function(inquireResponse) {
+	console.log(inquireResponse.menu);
+	runOption(inquireResponse.menu);
 });
 
 function runOption(userResponse) {
 	if(userResponse == 'View Products for Sale') {
-
-		viewProducts();
-
+		 displayProducts(viewInventoryQuery, viewInventoryMsg);
+		// viewProducts();
 	} else if (userResponse == 'View Low Inventory') {
-		
-		viewLowInventory();
-
+		 displayProducts(viewLowInventoryQuery, viewLowInventoryMsg);
+		// viewLowInventory();
 	}else if (userResponse == 'Add to Inventory') {
 
 		addToInventory();
-
 	}else if (userResponse == 'Add New Product') {
+		
 		addNewProduct();
 	}
 }
 
-// * If a manager selects `View Products for Sale`, 
-// the app should list every available item: the item IDs, names, prices, and quantities.
+// // this function will run a query to view all the products available.
+// function viewProducts() {
+// 	connection.query('SELECT item_id, product_name, department_name, price, stock_quantity '+
+// 	'FROM products', function(err,res) {
+// 		if(err) throw err;
+// 		console.log('----------- Current Items -----------')
+// 		console.log(res);
+// 		connection.end();
+// 	});
 
-// this function will run a query to view all the products available.
-function viewProducts() {
-	
-	connection.query('SELECT item_id, product_name, department_name, price, stock_quantity '+
-	'FROM products', function(err,res) {
+// // this function will run a query to get all the Low in inventory
+// function viewLowInventory() {
+// 	connection.query('SELECT * FROM products WHERE stock_quantity <= 200', function(err,res) {
+// 		if(err) throw err;
+// 		console.log('----------- Current Items Low in Inventory -----------')
+// 		console.log(res);
+// 		connection.end();	
+// 	});
+// }
+
+function displayProducts(query, log) {
+	connection.query(query, function(err,res) {
 		if(err) throw err;
-		console.log('----------- Current Items -----------')
-		console.log(res);
-		connection.end();
-	});
-}
-
-  
-// this function will run a query to get all the Low in inventory
-function viewLowInventory() {
-
-	connection.query('SELECT * FROM products WHERE stock_quantity <= 200', function(err,res) {
-		if(err) throw err;
-		console.log('----------- Current Items Low in Inventory -----------')
+		console.log(log);
 		console.log(res);
 		connection.end();	
 	});
@@ -76,9 +83,7 @@ function viewLowInventory() {
 // This function allows the user to add the ID of the item
 // they wish to add to.
 function addToInventory() {
-
-	connection.query('SELECT * FROM products', function(err,res){
-
+	connection.query('SELECT * FROM products', function(err,res) {
 		if(err) throw err;
 		console.log(res);
 		inquirer.prompt([
@@ -87,12 +92,12 @@ function addToInventory() {
 				message: 'enter the Item Id you would like to add more to.',
 				name: 'addInvId'
 			}
-			]).then(function(inquireResponse) {
-				
-				var id = inquireResponse.addInvId
-				idValidCheck(id);
+		]).then(function(inquireResponse) {
+			
+			var id = inquireResponse.addInvId;
+			idValidCheck(id);
 
-			});
+		});
 
 	});
 	
@@ -102,116 +107,108 @@ function addToInventory() {
 // If the Id is invalid, then we bring the user back to prior prompt
 // If it was a valid input then Ask how much would they like to add
 function idValidCheck(responseId) {
-
 	connection.query('SELECT stock_quantity, product_name, price FROM products WHERE item_id = '
 	 + responseId,
-	 	function(err,res) {
-	 			
-	 			if(res == '' || res == null) {
-	 				
-	 				inquirer.prompt([
-						{
-							type: 'input',
-							message: 'You entered an invalid Id number.'+
-							' Enter anything to re-submit an Id number. Or Type "exit" to exit',
-							name: 'addInvId'
-						}
-						]).then(function(response) {
-							
-							if(response.addInvId == 'exit') {
-								
-								connection.end();
+	 	function(err,res) {	 			
+ 			if(err) throw err;
+			if(res == '' || res == null) { 				
+ 				inquirer.prompt([
+					{
+						type: 'input',
+						message: 'You entered an invalid Id number.'+
+						' Enter anything to re-submit an Id number. Or Type "exit" to exit',
+						name: 'addInvId'
+					}
+				]).then(function(response) {				
+					if(response.addInvId == 'exit') {
+						
+						connection.end();
+					} else {
+						
+						addToInventory();
+					}
+				})
+			} else if (res != '') {
+				console.log('=====================');
+				inquirer.prompt([
+					{
+						type: 'input',
+						message: 'How much '+res[0].product_name+' would you like to add?',
+						name: 'amount'
+					}
+				]).then(function(inquireResponse) {					
+					var amount = parseInt(inquireResponse.amount);
+					var id     = responseId;
+					var stock  = parseInt(res[0].stock_quantity);
+					stock = amount + stock;
 
-							} else {
-								
-								addToInventory();
-
-							}
-							
-						})
-
-				} else if (res != '') {
-					console.log('=====================');
-					inquirer.prompt([
-						{
-							type: 'input',
-							message: 'How much '+res[0].product_name+' would you like to add?',
-							name: 'amount'
-						}
-						]).then(function(inquireResponse){
-							
-							var amount = parseInt(inquireResponse.amount);
-							var id     = responseId;
-							var stock  = parseInt(res[0].stock_quantity);
-							stock = amount + stock;
-
-							updateProduct(responseId, stock);
-							
-						})
-				}
-	 	});
+					updateProduct(responseId, stock);
+				})
+			}
+		}
+	);
 }
 
 // this is the update function. This will actually update the table
 // based on the item_id and amount wished to be updated.
 function updateProduct(itemId,amount) {
-
 	connection.query('UPDATE products SET stock_quantity = '+amount+
 		' WHERE item_id = ' + itemId ,function(err,res) {
-
 			if(err) throw err;
+
 			console.log('=====================');
 			console.log('Update Successful!');
 			connection.end();	
-		})
+		}
+	);
 }
 
 function addNewProduct() {
-
 	inquirer.prompt([
-		{
-			type: 'input',
-			message: 'Enter the Item Id',
-			name: 'item_id'
-		},
-		{
-			type: 'input',
-			message: 'Enter the Product Name',
-			name: 'product_name'
-		},
-		{
-			type: 'input',
-			message: 'Enter the Department Name',
-			name: 'department_name'
-		},
-		{
-			type: 'input',
-			message: 'Enter the Price',
-			name: 'item_price'
-		},
-		{
-			type: 'input',
-			message: 'Enter the Amount that will be in stock',
-			name: 'stock_quantity'
-		}
-		]).then(function(inquireResponse) {
-			var id 			= parseInt(inquireResponse.item_id);
-			var name 		= inquireResponse.product_name;
-			var department 	= inquireResponse.department_name;
-			var price 		= parseFloat(inquireResponse.item_price);
-			var quantity 	= parseInt(inquireResponse.stock_quantity);
-			var query 		= 'INSERT INTO products (item_id, product_name, department_name,'+
-				' price, stock_quantity) VALUES (' + id + ' , "' + name + '", "' + department
-				+ '", ' + price + ', ' + quantity + ')';
+			{
+				type: 'input',
+				message: 'Enter the Item Id',
+				name: 'item_id'
+			},
+			{
+				type: 'input',
+				message: 'Enter the Product Name',
+				name: 'product_name'
+			},
+			{
+				type: 'input',
+				message: 'Enter the Department Name',
+				name: 'department_name'
+			},
+			{
+				type: 'input',
+				message: 'Enter the Price',
+				name: 'item_price'
+			},
+			{
+				type: 'input',
+				message: 'Enter the Amount that will be in stock',
+				name: 'stock_quantity'
+			}
+	]).then(function(inquireResponse) {	
+		var id 			= parseInt(inquireResponse.item_id);
+		var name 		= inquireResponse.product_name;
+		var department 	= inquireResponse.department_name;
+		var price 		= parseFloat(inquireResponse.item_price);
+		var quantity 	= parseInt(inquireResponse.stock_quantity);
+		var query 		= 'INSERT INTO products (item_id, product_name, department_name,'+
+			' price, stock_quantity, product_sales) VALUES (' + id + ' , "' + name + '", "' + department
+			+ '", ' + price + ', ' + quantity + ', ' + 0 + ')';
 
-			connection.query(query, function(err, res) {
+		connection.query(query,function(err, res) {
 
-					if(err) throw err;
-					console.log(res);
-					console.log('Item Inserted!');	
-					connection.end();	
-				})
-		})
+				if(err) throw err;
+
+				console.log(res);
+				console.log('Item Inserted!');	
+				connection.end();	
+		});
+	});
 }	
 
 
